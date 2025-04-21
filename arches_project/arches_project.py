87c0e4ef1a3090e46d2f92214c4b1ff0c5b1cadc
Samuel Scandrett <samuel.scandrett@k-int.co.uk>
2025-04-21 18:35:59 +0100

    sort import and add note for permanent message

--- arches_project/arches_project.py
+++ arches_project/arches_project.py
@@ -51,8 +51,7 @@ from .ui.create_resource_confirmation_dialog import CreateResourceConfirmation
 from .ui.edit_resource_add_confirmation_dialog import EditResourceAddConfirmation
 from .ui.edit_resource_replace_confirmation_dialog import EditResourceReplaceConfirmation
 
-from .core.arches.connection import ArchesConnection
-from .core.arches.connection import ConnectionProcess
+from .core.arches.connection import ArchesConnection, ConnectionProcess
 from .core.arches.resources import ArchesResources
 
 from .core.views.stylesheets import PluginStylesheets
