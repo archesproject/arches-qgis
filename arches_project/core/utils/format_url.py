f4e1bb9c71d41a792b64358ad79d4aa46ef963e9
Samuel Scandrett <119508494+samuelscandrett@users.noreply.github.com>
2025-09-03 00:26:18 +0100

    add placeholder for i18n and explain url function (#43)

--- arches_project/core/utils/format_url.py
+++ arches_project/core/utils/format_url.py
@@ -1,6 +1,11 @@
 def format_url(url_input):
-    # formatted_url = self.dlg.archesServerInput.text().strip()
+    """
+    Simple function to fix any possible user input errors.
+    Strips any leading/trailing whitespace and a trailing slash.
+    """
+
     formatted_url = url_input.strip()
     if formatted_url[-1] == "/":
         formatted_url = formatted_url[:-1]
+
     return formatted_url
