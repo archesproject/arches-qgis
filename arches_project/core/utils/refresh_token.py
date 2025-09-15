4dee445a1834eef56f938a0d3ae91c43fb890c4a
CWDamm-Kint <chris.damm@k-int.co.uk>
2025-09-15 11:50:49 +0100

    swap arches_token for archesproject.arches_token

--- arches_project/core/utils/refresh_token.py
+++ arches_project/core/utils/refresh_token.py
@@ -2,24 +2,21 @@ from datetime import datetime, timedelta
 import requests
 
 def refresh_token(archesproject):
-    arches_token = archesproject.arches_token
 
     try:
         files = {
         'grant_type': (None, "refresh_token"),
         'client_id': (None, archesproject.clientid),
-        'refresh_token': (None, arches_token['refresh_token']),
+        'refresh_token': (None, archesproject.arches_token['refresh_token']),
         }
 
-        response = requests.post(arches_token["formatted_url"] +"/o/token/", data=files, timeout=10)
+        response = requests.post(archesproject.arches_token["formatted_url"] +"/o/token/", data=files, timeout=10)
         refreshed_token = response.json()
 
-        arches_token["time"] = datetime.now()
-        arches_token['access_token'] = refreshed_token['access_token']
-        arches_token["refresh_token"] = refreshed_token["refresh_token"]
-        arches_token["expires_at"] = arches_token["time"] + timedelta(seconds=refreshed_token["expires_in"])
-
-        archesproject.arches_token = arches_token
+        archesproject.arches_token["time"] = datetime.now()
+        archesproject.arches_token['access_token'] = refreshed_token['access_token']
+        archesproject.arches_token["refresh_token"] = refreshed_token["refresh_token"]
+        archesproject.arches_token["expires_at"] = archesproject.arches_token["time"] + timedelta(seconds=refreshed_token["expires_in"])
 
     except Exception as e:
         print(f"Failed to get OAuth token: {e}")
