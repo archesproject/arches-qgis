50ce3b209835035172fa06372a8ea82905a1a882
Samuel Scandrett <samuel.scandrett@k-int.co.uk>
2025-09-30 23:56:09 +0100

    Add Arches docker container solution for testing

---
+++ test/arches/settings_local.py
@@ -0,0 +1,36 @@
+import os
+from django.core.exceptions import ImproperlyConfigured
+
+try:
+    from .arches_qgis_project.settings import *
+except ImportError:
+    pass
+
+def get_env_variable(var_name):
+    msg = "Set the %s environment variable"
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        error_msg = msg % var_name
+        raise ImproperlyConfigured(error_msg)
+
+
+ELASTICSEARCH_HOSTS = [
+    {
+        "scheme": "http",
+        "host": get_env_variable("ESHOST"),
+        "port": int(get_env_variable("ESPORT_CONTAINER")),
+    }
+]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.contrib.gis.db.backends.postgis",
+        "NAME": get_env_variable("ARCHES_PROJECT"),
+        "USER": get_env_variable("PGUSERNAME"),
+        "PASSWORD": get_env_variable("PGPASSWORD"),
+        "HOST": get_env_variable("PGHOST"),
+        "PORT": get_env_variable("PGPORT_CONTAINER"),
+        "POSTGIS_TEMPLATE": "template_postgis",
+    }
+}
