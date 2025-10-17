da252d6275a2b98cc912f345805cc3cdf5f60f58
Samuel Scandrett <samuel.scandrett@k-int.co.uk>
2025-10-18 00:17:26 +0100

    Add test for access token

--- arches_project/tests/test_core/test_connection.py
+++ arches_project/tests/test_core/test_connection.py
@@ -13,14 +13,26 @@ CANVAS, PARENT, IFACE, QGIS_APP = get_qgis_app()
 
 class ConnectionTests(ArchesQGISTestCase):
     """
-    Test connection to Arches.
+    Test connection to Arches server.
     """
 
-    def test_get_clientid(self):
+    client_id = "ZmRsVUmUtwas8lmgX40PmgAQacESxxv9EPQdIm8S"
 
+    def test_get_clientid(self):
         files = {
             "username": (None, "admin"),
             "password": (None, "admin"),
         }
         response = requests.post(f"{self.arches_url}/auth/get_client_id", data=files)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()["clientid"], self.client_id)
+
+    def test_access_token(self):
+        files = {
+            "username": (None, "admin"),
+            "password": (None, "admin"),
+            "client_id": (None, self.client_id),
+            "grant_type": (None, "password"),
+        }
+        response = requests.post(self.arches_url + "/o/token/", data=files)
         self.assertEqual(response.status_code, 200)
