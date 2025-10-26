e2b20c88c07cbd0d00479be4731cb70c243bd45e
Samuel Scandrett <samuel.scandrett@k-int.co.uk>
2025-10-26 23:24:08 +0000

    Move geometry_nodes list for multiple into arches api

--- arches_project/core/views/components/multiple_graph_nodes.py
+++ arches_project/core/views/components/multiple_graph_nodes.py
@@ -5,14 +5,13 @@ def multiple_geometry_node_check(dlg):
     selectedGraphIndex = dlg.createResModelSelect.currentIndex()
     selectedGraph = arches_api.arches_graphs_list[selectedGraphIndex]
 
-    geometry_nodes = []
     dlg.geometryNodeSelect.setEnabled(False)
     dlg.geometryNodeSelectFrame.hide()
 
     if selectedGraph:
         if selectedGraph["multiple_geometry_nodes"] == True:
             for k, v in selectedGraph["geometry_node_data"].items():
-                geometry_nodes.append(
+                arches_api.geometry_nodes.append(
                     {
                         "node_id": k,
                         "nodegroup_id": v["nodegroup_id"],
@@ -21,5 +20,5 @@ def multiple_geometry_node_check(dlg):
                 )
             dlg.geometryNodeSelect.setEnabled(True)
             dlg.geometryNodeSelect.clear()
-            dlg.geometryNodeSelect.addItems([n["name"] for n in geometry_nodes])
+            dlg.geometryNodeSelect.addItems([n["name"] for n in arches_api.geometry_nodes])
             dlg.geometryNodeSelectFrame.show()
