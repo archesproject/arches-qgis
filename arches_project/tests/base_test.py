d94f27bcf135490259c0dc545a34d0f650d057ed
Samuel Scandrett <119508494+samuelscandrett@users.noreply.github.com>
2025-11-10 21:24:05 +0000

    Fix PyQt debug messages when running tests (#96)
    * Remove run from base test
    
    * Remove invalid length property from arches qss

--- arches_project/tests/base_test.py
+++ arches_project/tests/base_test.py
@@ -46,7 +46,6 @@ class ArchesQGISTestCase(unittest.TestCase):
 
         self.arches_project.first_start = True
         self.arches_project.initGui()
-        self.arches_project.run()
 
     def tearDown(self):
         """
