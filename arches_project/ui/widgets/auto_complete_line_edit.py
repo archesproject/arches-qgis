1565eebaeb35358ea41df0d816ea50b5885792c6
CWDamm-Kint <chris.damm@k-int.co.uk>
2025-11-12 09:44:44 +0000

    make AutoCompleteLineEdit a sub class, plus formatting

---
+++ arches_project/ui/widgets/auto_complete_line_edit.py
@@ -0,0 +1,12 @@
+from qgis.PyQt.QtWidgets import QLineEdit
+
+
+class AutoCompleteLineEdit(QLineEdit):
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+    def focusInEvent(self, event):
+        super().focusInEvent(event)
+        completer = self.completer()
+        completer.complete()
