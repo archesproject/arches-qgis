c85c5efecb68bf283e0d97f2eaf3911a52d43761
Samuel Scandrett <samuel.scandrett@k-int.co.uk>
2025-12-16 11:47:16 +0000

    Create new widgets for map selection information

--- arches_project/ui/arches_project_dialog.py
+++ arches_project/ui/arches_project_dialog.py
@@ -160,23 +160,9 @@ class ArchesProjectDialog(QtWidgets.QDialog, FORM_CLASS):
             )
         )
 
-        # hide loading wheel adjustments
-        self.loadingWheel.hide()  # connection wheel label
-        self.updateTextFrame.hide()
-        self.loginErrorMessageFrame.hide()
-        self.loadingWheelVerticalSpacerFrame.hide()
-
-        # Stylesheets
-        self.stylesheets = Stylesheets(
-            dlg=self,
-            dlg_resource_confirmation=self.dlg_resource_confirmation,
-            plugin_dir=self.plugin_dir,
-        )
-
-        self.useStylesheetCheckbox.stateChanged.connect(
-            partial(
-                self.stylesheets.stylesheet_changed,
-            )
+        # Change mouse to select from map
+        self.createResFeatureSelectButton.clicked.connect(
+            self.iface.actionSelect().trigger
         )
 
         self.opacity_effect = QGraphicsOpacityEffect(self.refreshConfirmLabel)
