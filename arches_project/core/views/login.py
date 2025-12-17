b78871b395a5c3e1018ec0f87d460a575ce28da7
Samuel Scandrett <samuel.scandrett@k-int.co.uk>
2025-12-17 09:33:09 +0000

    Move reset & update views into separate file, improve login class functions

--- arches_project/core/views/login.py
+++ arches_project/core/views/login.py
@@ -12,6 +12,9 @@ class LoggedIn:
         self.url = url
 
     def update_logged_in_view(self):
+        """
+        Update the "logged in" tab one logged in
+        """
         full_name = ""
         if arches_api.arches_user_info["first_name"]:
             full_name = f'{arches_api.arches_user_info["first_name"]} {arches_api.arches_user_info["last_name"]}'
@@ -26,6 +29,14 @@ class LoggedIn:
             self.dlg.displayUsernameLabel.setText(self.username)
 
         self.dlg.displayConnectionInfoLabel.setText(f"You are connected to {self.url}.")
+
+    def hide_login_tab(self):
+        """
+        Hide the login tab and reveal the logged in/user profile tab
+        """
+        self.dlg.tabWidget.setTabVisible(0, False)
+        self.dlg.tabWidget.setTabVisible(1, True)
+        self.dlg.tabWidget.setCurrentIndex(1)
 
 
 class UpdateLoginProgress:
