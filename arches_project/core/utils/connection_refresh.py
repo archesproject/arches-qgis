cf61589bfa871a5abad47e9a0038bb22e5ded4f6
CWDamm-Kint <chris.damm@k-int.co.uk>
2025-12-22 16:59:35 +0000

    make connection refresh into a task, plus a helper function to run it

--- arches_project/core/utils/connection_refresh.py
+++ arches_project/core/utils/connection_refresh.py
@@ -1,31 +1,7 @@
-from arches_project.core.arches.connection import ArchesConnection
-from arches_project.core.arches.api import arches_api
-from arches_project.core.views.components.dialog_updates import (
-    update_refresh_confirm_label,
-    update_create_resources_tab,
-    connection_reset,
-    update_edit_resources_tab,
-)
-from arches_project.core.views.components.qgis_messaging import show_message
-import requests
+from qgis.core import QgsApplication
+from arches_project.core.arches.ConnectionRefreshTask import ConnectionRefreshTask
 
 
-def connection_refresh(dlg, iface):
-
-    url = arches_api.arches_connection_cache["url"]
-    arches_connection = ArchesConnection(url, None, None)
-
-    try:
-        arches_api.arches_graphs_list = arches_connection.get_graphs()
-        update_refresh_confirm_label(dlg=dlg, connected=True)
-        update_create_resources_tab(dlg=dlg)
-        update_edit_resources_tab(dlg=dlg)
-    except requests.exceptions.RequestException:
-        update_refresh_confirm_label(dlg=dlg, connected=False)
-        connection_reset(hard_reset=False, dlg=dlg, iface=iface, manual_logout=False)
-        show_message(
-            iface,
-            "Error",
-            "Failed to reconnect to Arches instance.",
-            duration=-1,
-        )
+def connection_refresh(dlg):
+    dlg.connection_refresh_task = ConnectionRefreshTask(dlg=dlg, iface=dlg.iface)
+    QgsApplication.taskManager().addTask(dlg.connection_refresh_task)
