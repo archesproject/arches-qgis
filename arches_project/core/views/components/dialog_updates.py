422069cd7333f43389964695740b3725df109e60
Chris <chris.damm@k-int.co.uk>
2025-12-30 14:53:33 +0000

    Clear login fields only if logging out (#137)
    * clear login fields only if logging out
    
    * require explicit True value for manual_logout

--- arches_project/core/views/components/dialog_updates.py
+++ arches_project/core/views/components/dialog_updates.py
@@ -12,10 +12,6 @@ def connection_reset(hard_reset, dlg, iface, manual_logout=False):
     Reset Arches connection
     """
     if hard_reset == True:
-        # Reset connection inputs
-        dlg.archesServerInput.setText("")
-        dlg.usernameInput.setText("")
-        dlg.passwordInput.setText("")
         # Reset logged in values
         dlg.displayFullNameLabel.setText("")
         dlg.displayConnectionInfoLabel.setText("")
@@ -24,6 +20,11 @@ def connection_reset(hard_reset, dlg, iface, manual_logout=False):
         dlg.tabWidget.setTabVisible(0, True)
         dlg.tabWidget.setTabVisible(1, False)
         dlg.tabWidget.setCurrentIndex(0)
+        # Reset connection inputs
+        if manual_logout == True:
+            dlg.archesServerInput.setText("")
+            dlg.usernameInput.setText("")
+            dlg.passwordInput.setText("")
 
     # Reset stored data
     arches_api.arches_user_info = {}
@@ -52,7 +53,7 @@ def connection_reset(hard_reset, dlg, iface, manual_logout=False):
     # Reload saved credentials for the autocompletes
     load_saved_credentials(dlg)
 
-    if manual_logout:
+    if manual_logout == True:
         show_message(
             iface,
             "information",
