326a28610d5abb275a371bf8d2f8c60cc9420500
CWDamm-Kint <chris.damm@k-int.co.uk>
2026-01-05 10:10:33 +0000

    remove enable logging checkbox

--- arches_project/core/views/logging.py
+++
@@ -1,9 +0,0 @@
-def enable_logging(self):
-    if self.dlg.enableLoggingCheckbox.isChecked():
-        self.dlg.tabWidget.setTabVisible(5, True)
-
-    elif not self.dlg.enableLoggingCheckbox.isChecked():
-        self.dlg.tabWidget.setTabVisible(5, False)
-
-
-# TODO needs expanding into class and functionality
