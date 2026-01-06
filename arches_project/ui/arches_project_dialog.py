e57610db7d8da4858b7270d19172acd463f46512
CWDamm-Kint <chris.damm@k-int.co.uk>
2026-01-06 12:17:17 +0000

    resize table

--- arches_project/ui/arches_project_dialog.py
+++ arches_project/ui/arches_project_dialog.py
@@ -183,6 +183,6 @@ class ArchesProjectDialog(QtWidgets.QDialog, FORM_CLASS):
         self.opacity_effect = QGraphicsOpacityEffect(self.refreshConfirmLabel)
         self.refreshConfirmLabel.setGraphicsEffect(self.opacity_effect)
         # set initial log table widths
-        self.activityLogTable.setColumnWidth(0, 110)
-        self.activityLogTable.setColumnWidth(1, 125)
+        self.activityLogTable.setColumnWidth(0, 100)
+        self.activityLogTable.setColumnWidth(1, 100)
         self.activityLogTable.horizontalHeader().setStretchLastSection(True)
